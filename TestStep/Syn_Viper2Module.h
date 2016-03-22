ff39e08ac04a2022942488ff5ac6b2707bccdafb
unknown <jezhang@SHA-JZ-LT01.synaptics-inc.local>
2016-03-22 22:28:32 +0800

    add ImageDecode for viper2

--- TestStep/Syn_Viper2Module.h
+++ TestStep/Syn_Viper2Module.h
@@ -6,6 +6,12 @@ public:
 	Syn_Viper2Module();
 	virtual ~Syn_Viper2Module();
 
+	void ImageDecode(FPSFrame *pDecodeFrame, FPSFrame *pEncodeFrame, int nNumRow, int nNumCol, int nNumFrames);
+
 	bool CalculatePgaOffsets_OOPP(uint16_t numCols, uint16_t numRows, CalibrationInfo &calInfo, CalibrationResults &calResult);
+
+private:
+	//CDM16 conversion	
+	static float m_matrix[144][144];
 };
 
