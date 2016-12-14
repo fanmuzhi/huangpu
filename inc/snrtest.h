8f27d8b8b3020bd7b340fa93cec74614ae7d9d75
boqiling <boqiling@gmail.com>
2016-12-14 22:05:41 +0800

    add latest SNR test

--- inc/snrtest.h
+++ inc/snrtest.h
@@ -12,6 +12,8 @@
 // 	};
 // }
 extern "C" __declspec(dllexport) int __stdcall calchuaidian(unsigned char* pImage, int iWidth, int iHeight,int* num_of_bubble,float fGrav);
-extern "C" __declspec(dllexport) float __stdcall testSNRUG(unsigned char* pWiteimg,unsigned char* pSpoofimg,int iWidth,int iHeight,int iNumofimg);
+extern "C" __declspec(dllexport) float __stdcall testSNRUG(unsigned char* pWiteimg,unsigned char* pSpoofimg,int iWidth,int iHeight,float *signalout,float* noiseout,int iNumofimg);
 extern "C" __declspec(dllexport) float __stdcall testSNR(unsigned char* pImagewhite30, unsigned char* pImagespoof30,int width,int height, int iNumofimg,int *signal,float *noise,float fcrop);
 extern "C" __declspec(dllexport) int __stdcall add(int x,int y);
+extern "C" __declspec(dllexport) int __stdcall CalTilt(unsigned char* pImage,int width ,int height ,int *whole,int *block);
+extern "C" __declspec(dllexport) int __stdcall getAlgoVersion();
