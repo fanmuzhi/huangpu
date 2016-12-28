79506c90972c5033cd020c50fb79ee8baa756b85
justinli <justin.li@cn.synaptics.com>
2016-12-28 08:15:20 +0800

    modify return mode temp

--- TestStep/Ts_RetainMode.cpp
+++ TestStep/Ts_RetainMode.cpp
@@ -29,6 +29,7 @@ void Ts_RetainMode::SetUp()
 		throw ex;
 		return;
 	}
+
 	//parse args
 	std::vector<std::string> listOfArgValue;
 	_pSyn_Dut->_pSyn_DutTestInfo->_retainModeInfo.m_bExecuted = false;
@@ -52,24 +53,15 @@ void Ts_RetainMode::SetUp()
 		_pSyn_Dut->_pSyn_DutTestInfo->_retainModeInfo.m_nMinCurrent = (float)atof(listOfArgValue[2].c_str());
 	if (0 != listOfArgValue[2].length())
 		_pSyn_Dut->_pSyn_DutTestInfo->_retainModeInfo.m_nDelay = atoi(listOfArgValue[3].c_str());
-
-	//load WofPatch
-	Syn_PatchInfo WofPatchInfo;
-	if (!_pSyn_Dut->FindPatch("WofPatch", WofPatchInfo) || NULL == WofPatchInfo._pArrayBuf)
-	{
-		ex.SetError(Syn_ExceptionCode::Syn_DutPatchError);
-		ex.SetDescription("WofPatchInfo Patch is NULL!");
-		throw ex;
-		return;
-	}
-	_pSyn_DutCtrl->FpLoadPatch(WofPatchInfo._pArrayBuf, WofPatchInfo._uiArraySize);
 	
 }
 
 
 void Ts_RetainMode::Execute()
 {
+	uint32_t rc(0);
 	Syn_Exception ex(0);
+
 	if (NULL == _pSyn_DutCtrl)
 	{
 		ex.SetError(Syn_ExceptionCode::Syn_DutCtrlNull);
@@ -84,6 +76,67 @@ void Ts_RetainMode::Execute()
 		throw ex;
 		return;
 	}
+
+	//load WofPatch
+	Syn_PatchInfo WofPatchInfo;
+	if (!_pSyn_Dut->FindPatch("WofPatch", WofPatchInfo) || NULL == WofPatchInfo._pArrayBuf)
+	{
+		ex.SetError(Syn_ExceptionCode::Syn_DutPatchError);
+		ex.SetDescription("WofPatchInfo Patch is NULL!");
+		throw ex;
+	}
+
+	rc = _pSyn_DutCtrl->FpLoadPatch(WofPatchInfo._pArrayBuf, WofPatchInfo._uiArraySize);
+	if (0 != rc)
+	{
+		ex.SetError(rc);
+		ex.SetDescription("Load Wof Patch Failed");
+		throw ex;
+	}
+
+	rc = _pSyn_DutCtrl->FpTidleSet(0x03e8);
+	if (0 != rc)
+	{
+		ex.SetError(rc);
+		ex.SetDescription("Enter Sleep Mode Failed");
+		throw ex;
+	}
+
+	//get current
+	uint32_t arrValue[2] = { 0, 0 };
+	rc = _pSyn_DutCtrl->FpTidleSet(0x03e8);
+
+	////1st poke
+	//uint32_t nData;
+	//uint32_t pDst;
+	//_pSyn_DutCtrl->FpPeekRegister(0x80002808, 4, pDst);
+	//_pSyn_DutCtrl->FpPeekRegister(0x80002814, 4, pDst);
+	//_pSyn_DutCtrl->FpPokeRegister(0x80002808, 0x3);
+	//_pSyn_DutCtrl->FpPeekRegister(0x80002814, 4, pDst);
+	//_pSyn_DutCtrl->FpPokeRegister(0x80002814, 0x20);
+	//_pSyn_DutCtrl->FpPokeRegister(0x80002814, 0x20);
+	//_pSyn_DutCtrl->FpPeekRegister(0x80002814, 4, pDst);
+	//_pSyn_DutCtrl->FpPokeRegister(0x80002814, 0x21);
+	//_pSyn_DutCtrl->FpPokeRegister(0x80002064, 0x10000000);
+	//_pSyn_DutCtrl->FpPokeRegister(0x80002064, 0x6F20000); 
+	//_pSyn_DutCtrl->FpPeekRegister(0x80002064, 4 , pDst);
+	//_pSyn_DutCtrl->FpPokeRegister(0x80002064, 0x20000007);
+
+	////2nd poke
+	//_pSyn_DutCtrl->FpPokeRegister(0x8000030C, 0x41);
+	//_pSyn_DutCtrl->FpPokeRegister(0x80000388, 0x07D0);
+	//_pSyn_DutCtrl->FpPokeRegister(0x8000038C, 0x401008);
+	//_pSyn_DutCtrl->FpPokeRegister(0x80000390, 0xFFFF00C4);
+	//_pSyn_DutCtrl->FpPokeRegister(0x80000394, 0x0);
+	//_pSyn_DutCtrl->FpPokeRegister(0x80000300, 0x19, false);
+
+	_pSynBridge->GetCurrentValues(arrValue, true);		//low gain
+	
+	float spivcc_current = ((float)(arrValue[0]) - (float)(_pSyn_Dut->_pSyn_DutTestInfo->_adcBaselineInfo.m_arrAdcBaseLines[0])) / 1000;
+	float vcc_current = ((float)(arrValue[1]) - (float)(_pSyn_Dut->_pSyn_DutTestInfo->_adcBaselineInfo.m_arrAdcBaseLines[1])) / 1000;
+
+	_pSyn_Dut->_pSyn_DutTestResult->_deepSleepCurrentResults.spivcc_current_uA = spivcc_current;	//uA
+	_pSyn_Dut->_pSyn_DutTestResult->_deepSleepCurrentResults.vcc_current_uA = vcc_current;		//uA
 	//_pSyn_DutCtrl->FpEnterSleep();
 
 	//uint16_t nADCBaseline = site.GetLocalSettingsPtr()->GetAblInfo(site.GetDutCtrlSerNum()).m_arAdcBaseLines[2][2];//baseline gain.
@@ -162,5 +215,21 @@ void Ts_RetainMode::ProcessData()
 
 void Ts_RetainMode::CleanUp()
 {
-	_pSyn_DutCtrl->FpUnloadPatch();
+	Syn_Exception ex(0);
+	uint32_t rc = _pSyn_DutCtrl->FpReset();
+	if (0 != rc)
+	{
+		ex.SetError(rc);
+		ex.SetDescription("FpReset() Failed");
+		throw ex;
+	}
+
+	rc = _pSyn_DutCtrl->FpTidleSet(0);
+	if (0 != rc)
+	{
+		ex.SetError(rc);
+		ex.SetDescription("FpTidleSet command failed!");
+		throw ex;
+		return;
+	}
 }
